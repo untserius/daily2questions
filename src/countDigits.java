f2e270ace5f7c6aad05f888d2bb6cb0cdadff007
Sudhir Das <sudhirdas1999@gmail.com>
2023-10-23 12:44:11 +0530

    files added

---
+++ src/countDigits.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class countDigits {
+    public static void main(String[] args) {
+        int n = 0;
+
+        System.out.println(noOfDigits(n));
+    }
+
+    static int noOfDigits(int n){
+        int x = n;
+        int count = 0;
+
+        if (x != 0) {
+            while (x != 0) {
+                x = x / 10;
+                count++;
+            }
+            return count;
+        }
+        return 1;
+    }
+}
